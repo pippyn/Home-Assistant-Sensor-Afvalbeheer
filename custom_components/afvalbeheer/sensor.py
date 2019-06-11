1787549410c846ac82ebd5af4d2d24f9fb7e24c4
pippyn <pippyn@users.noreply.github.com>
2019-06-11 14:04:40 +0200

    Update sensor.py

--- custom_components/afvalbeheer/sensor.py
+++ custom_components/afvalbeheer/sensor.py
@@ -1,7 +1,7 @@
 """
 Sensor component for waste pickup dates from dutch waste collectors (using the http://www.opzet.nl app)
 Original Author: Pippijn Stortelder
-Current Version: 2.2.5 20190313 - Pippijn Stortelder
+Current Version: 2.3.1 20190313 - Pippijn Stortelder
 20190116 - Merged different waste collectors into 1 component
 20190119 - Added an option to change date format and fixed spelling mistakes
 20190122 - Refactor code and bug fix
