72c2322bad03b52f9af574e71f014c773078a73a
Pippyn <pippyn@users.noreply.github.com>
2020-08-03 08:09:27 +0200

    Update sensor.py

--- custom_components/afvalbeheer/sensor.py
+++ custom_components/afvalbeheer/sensor.py
@@ -1,7 +1,7 @@
 """
 Sensor component for waste pickup dates from dutch and belgium waste collectors
 Original Author: Pippijn Stortelder
-Current Version: 4.6.0b4 20200730 - Pippijn Stortelder
+Current Version: 4.6.1 20200730 - Pippijn Stortelder
 20200419 - Major code refactor (credits @basschipper)
 20200420 - Add sensor even though not in mapping
 20200420 - Added support for DeAfvalApp
