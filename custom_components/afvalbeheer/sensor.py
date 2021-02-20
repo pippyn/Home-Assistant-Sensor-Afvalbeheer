3687aa256d0f48ec36700b81a2691c1039d3a5e7
Pippijn Stortelder <pippyn@users.noreply.github.com>
2021-02-20 16:15:47 +0100

    Fix wrong RecycleApp streetId

--- custom_components/afvalbeheer/sensor.py
+++ custom_components/afvalbeheer/sensor.py
@@ -1,7 +1,7 @@
 """
 Sensor component for waste pickup dates from dutch and belgium waste collectors
 Original Author: Pippijn Stortelder
-Current Version: 4.7.13 20210219 - Pippijn Stortelder
+Current Version: 4.7.14 20210220 - Pippijn Stortelder
 20200419 - Major code refactor (credits @basschipper)
 20200420 - Add sensor even though not in mapping
 20200420 - Added support for DeAfvalApp
@@ -64,6 +64,7 @@ Current Version: 4.7.13 20210219 - Pippijn Stortelder
 20210129 - Fix RecycleApp API access
 20210213 - Fix Meerlanden API url
 20210219 - Changed GFT mapping for RecycleApp
+20210220 - Fix wrong RecycleApp streetId
 
 Example config:
 Configuration.yaml:
@@ -1144,7 +1145,11 @@ class RecycleApp(WasteCollector):
         if not response.status_code == 200:
             _LOGGER.error('Invalid response from server for street_id')
             return
-        self.street_id = response.json()['items'][0]['id']
+        for item in response.json()['items']:
+            if item['name'] == self.street_name:
+                self.street_id = item['id']
+        if not self.street_id:
+            self.street_id = response.json()['items'][0]['id']
 
     def __get_data(self):
         startdate = datetime.now().strftime("%Y-%m-%d")
