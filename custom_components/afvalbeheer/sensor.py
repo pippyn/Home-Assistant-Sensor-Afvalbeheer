957a1cb9752138a850225d42ef463215fb9e28e3
Pippyn <pippyn@users.noreply.github.com>
2021-12-13 16:44:43 +0100

    Update sensor.py

--- custom_components/afvalbeheer/sensor.py
+++ custom_components/afvalbeheer/sensor.py
@@ -1,7 +1,7 @@
 """
 Sensor component for waste pickup dates from dutch and belgium waste collectors
 Original Author: Pippijn Stortelder
-Current Version: 4.8.0 20211213 - Pippijn Stortelder
+Current Version: 4.8.1 20211213 - Pippijn Stortelder
 20210112 - Updated date format for RD4
 20210114 - Fix error made in commit 9d720ec
 20210120 - Enabled textile for RecycleApp
@@ -33,6 +33,7 @@ Current Version: 4.8.0 20211213 - Pippijn Stortelder
 20211022 - Update Mijnafvalwijzer mapping
 20211212 - Replace device_state_attributes with extra_state_attributes
 20211213 - Breaking change: replaced - with _ in Days_until and Days_until
+20211213 - Add unique ids to all sensors
 
 Example config:
 Configuration.yaml:
