1850bd15febb7ce15c9da879cb1f2a429589ea61
Pippijn Stortelder <pippyn@users.noreply.github.com>
2022-10-19 09:14:47 +0200

    Fix old icons

--- custom_components/afvalbeheer/__init__.py
+++ custom_components/afvalbeheer/__init__.py
@@ -1,7 +1,7 @@
 """
 Sensor component for waste pickup dates from dutch and belgium waste collectors
 Original Author: Pippijn Stortelder
-Current Version: 5.0.5 202201019
+Current Version: 5.0.6 202201019
 20220829 - Major change: Added Calendar support (credits @WouterTuinstra)
 20220829 - Give persistant notifications unique id's
 20220901 - Code cleanup
@@ -45,7 +45,7 @@ from .const import DOMAIN, PLATFORM_SCHEMA, CONF_ID
 from .API import Get_WasteData_From_Config
 
 
-__version__ = "5.0.5"
+__version__ = "5.0.6"
 
 
 _LOGGER = logging.getLogger(__name__)
