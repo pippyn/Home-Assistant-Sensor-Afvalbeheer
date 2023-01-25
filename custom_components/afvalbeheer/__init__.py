c318dca19b0deb416766efa2948215c43be674ff
Pippijn Stortelder <pippyn@users.noreply.github.com>
2023-01-25 14:58:15 +0100

    Only add requested fractions to calendar

--- custom_components/afvalbeheer/__init__.py
+++ custom_components/afvalbeheer/__init__.py
@@ -1,7 +1,7 @@
 """
 Sensor component for waste pickup dates from dutch and belgium waste collectors
 Original Author: Pippijn Stortelder
-Current Version: 5.0.12 20230125
+Current Version: 5.0.13 20230125
 20220829 - Major change: Added Calendar support (credits @WouterTuinstra)
 20220829 - Give persistant notifications unique id's
 20220901 - Code cleanup
@@ -52,7 +52,7 @@ from .const import DOMAIN, PLATFORM_SCHEMA, CONF_ID
 from .API import Get_WasteData_From_Config
 
 
-__version__ = "5.0.12"
+__version__ = "5.0.13"
 
 
 _LOGGER = logging.getLogger(__name__)
