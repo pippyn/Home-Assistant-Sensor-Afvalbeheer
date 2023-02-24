3246bc7e0c05de67829a4e62419c42901fb7a44a
Pippijn Stortelder <pippyn@users.noreply.github.com>
2023-02-24 16:39:13 +0100

    Refactor code

--- custom_components/afvalbeheer/sensor.py
+++ custom_components/afvalbeheer/sensor.py
@@ -25,72 +25,17 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         schedule_update = True
         data = Get_WasteData_From_Config(hass, config)
 
-    waste_collector = config.get(CONF_WASTE_COLLECTOR).lower()
-    date_format = config.get(CONF_DATE_FORMAT)
     sensor_today = config.get(CONF_TODAY_TOMORROW)
-    date_object = config.get(CONF_DATE_OBJECT)
-    name = config.get(CONF_NAME)
-    name_prefix = config.get(CONF_NAME_PREFIX)
-    built_in_icons = config.get(CONF_BUILT_IN_ICONS)
-    built_in_icons_new = config.get(CONF_BUILT_IN_ICONS_NEW)
-    disable_icons = config.get(CONF_DISABLE_ICONS)
-    dutch_days = config.get(CONF_TRANSLATE_DAYS)
-    day_of_week = config.get(CONF_DAY_OF_WEEK)
-    day_of_week_only = config.get(CONF_DAY_OF_WEEK_ONLY)
-    always_show_day = config.get(CONF_ALWAYS_SHOW_DAY)
-
-    if date_object == True:
-        date_only = 1
-    else:
-        date_only = config.get(CONF_DATE_ONLY)
 
     entities = []
 
     for resource in config[CONF_RESOURCES]:
         waste_type = resource.lower()
-        entities.append(
-            WasteTypeSensor(
-                data,
-                waste_type,
-                waste_collector,
-                date_format,
-                date_only,
-                date_object,
-                name,
-                name_prefix,
-                built_in_icons,
-                built_in_icons_new,
-                disable_icons,
-                dutch_days,
-                day_of_week,
-                day_of_week_only,
-                always_show_day,
-            )
-        )
+        entities.append(WasteTypeSensor(data, waste_type, config))
 
     if sensor_today:
-        entities.append(
-            WasteDateSensor(
-                data,
-                config[CONF_RESOURCES],
-                waste_collector,
-                timedelta(),
-                dutch_days,
-                name,
-                name_prefix,
-            )
-        )
-        entities.append(
-            WasteDateSensor(
-                data,
-                config[CONF_RESOURCES],
-                waste_collector,
-                timedelta(days=1),
-                dutch_days,
-                name,
-                name_prefix,
-            )
-        )
+        entities.append(WasteDateSensor(data, config, timedelta()))
+        entities.append(WasteDateSensor(data, config, timedelta(days=1)))
 
     async_add_entities(entities)
 
@@ -100,30 +45,27 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
 
 class WasteTypeSensor(RestoreEntity, SensorEntity):
 
-    def __init__(self, data, waste_type, waste_collector, date_format, date_only, date_object,
-                name, name_prefix, built_in_icons, built_in_icons_new,disable_icons, dutch_days, 
-                day_of_week, day_of_week_only, always_show_day):
+    def __init__(self, data, waste_type, config):
         self.data = data
         self.waste_type = waste_type
-        self.waste_collector = waste_collector
-        self.date_format = date_format
-        self.date_only = date_only
-        self.date_object = date_object
-        self._name = _format_sensor(name, name_prefix, waste_collector, self.waste_type)
-        self._attr_unique_id = _format_sensor(name, name_prefix, waste_collector, self.waste_type)
-        self.built_in_icons = built_in_icons
-        self.built_in_icons_new = built_in_icons_new
-        self.disable_icons = disable_icons
-        self.dutch_days = dutch_days
-        self.day_of_week = day_of_week
-        self.day_of_week_only = day_of_week_only
-        self.always_show_day = always_show_day
-        if self.dutch_days:
-            self._today = "Vandaag"
-            self._tomorrow = "Morgen"
-        else:
-            self._today = "Today"
-            self._tomorrow = "Tomorrow"
+        self.waste_collector = config.get(CONF_WASTE_COLLECTOR).lower()
+        self.date_format = config.get(CONF_DATE_FORMAT)
+        self.date_object = config.get(CONF_DATE_OBJECT)
+        self.built_in_icons = config.get(CONF_BUILT_IN_ICONS)
+        self.built_in_icons_new = config.get(CONF_BUILT_IN_ICONS_NEW)
+        self.disable_icons = config.get(CONF_DISABLE_ICONS)
+        self.dutch_days = config.get(CONF_TRANSLATE_DAYS)
+        self.day_of_week = config.get(CONF_DAY_OF_WEEK)
+        self.day_of_week_only = config.get(CONF_DAY_OF_WEEK_ONLY)
+        self.always_show_day = config.get(CONF_ALWAYS_SHOW_DAY)
+        self.date_only = 1 if self.date_object else config.get(CONF_DATE_ONLY)
+
+        self._today = "Vandaag" if self.dutch_days else "Today"
+        self._tomorrow = "Morgen" if self.dutch_days else "Tomorrow"
+        
+        formatted_name = _format_sensor(config.get(CONF_NAME), config.get(CONF_NAME_PREFIX),  self.waste_collector, self.waste_type)
+        self._name = formatted_name
+        self._attr_unique_id = formatted_name
         self._days_until = None
         self._sort_date = 0
         self._hidden = False
@@ -175,11 +117,11 @@ class WasteTypeSensor(RestoreEntity, SensorEntity):
             if not self.disable_icons and 'entity_picture' in state.attributes.keys():
                 self._entity_picture = state.attributes['entity_picture']
             self._attrs = {
-            ATTR_WASTE_COLLECTOR: state.attributes[ATTR_WASTE_COLLECTOR],
-            ATTR_HIDDEN: state.attributes[ATTR_HIDDEN],
-            ATTR_SORT_DATE: state.attributes[ATTR_SORT_DATE],
-            ATTR_DAYS_UNTIL: state.attributes[ATTR_DAYS_UNTIL]
-        }
+                ATTR_WASTE_COLLECTOR: state.attributes[ATTR_WASTE_COLLECTOR],
+                ATTR_HIDDEN: state.attributes[ATTR_HIDDEN],
+                ATTR_SORT_DATE: state.attributes[ATTR_SORT_DATE],
+                ATTR_DAYS_UNTIL: state.attributes[ATTR_DAYS_UNTIL]
+            }
 
     def update(self):
         collection = self.data.collections.get_first_upcoming_by_type(self.waste_type)
@@ -199,9 +141,7 @@ class WasteTypeSensor(RestoreEntity, SensorEntity):
         date_format = self.date_format
         if self.date_object:
             self._state = collection.date
-        elif self.date_only:
-            self._state = collection.date.strftime(date_format)
-        elif date_diff >= 8 and not self.always_show_day:
+        elif self.date_only or (date_diff >= 8 and not self.always_show_day):
             self._state = collection.date.strftime(date_format)
         elif date_diff > 1:
             if self.day_of_week:
@@ -215,31 +155,14 @@ class WasteTypeSensor(RestoreEntity, SensorEntity):
             else:
                 self._state = collection.date.strftime(date_format)
         elif date_diff == 1:
-            if self.day_of_week_only:
-                self._state = collection.date.strftime(self._tomorrow)
-            else:
-                self._state = collection.date.strftime(self._tomorrow + ", " + date_format)
+            self._state = collection.date.strftime(self._tomorrow if self.day_of_week_only else self._tomorrow + ", " + date_format)
         elif date_diff == 0:
-            if self.day_of_week_only:
-                self._state = collection.date.strftime(self._today)
-            else:
-                self._state = collection.date.strftime(self._today + ", " + date_format)
+            self._state = collection.date.strftime(self._today if self.day_of_week_only else self._today + ", " + date_format)
         else:
             self._state = None
 
         if self.dutch_days and not self.date_object:
-            if "%B" in date_format:
-                for EN_day, NL_day in DUTCH_TRANSLATION_MONTHS.items():
-                    self._state = self._state.replace(EN_day, NL_day)
-            if "%b" in date_format:
-                for EN_day, NL_day in DUTCH_TRANSLATION_MONTHS_SHORT.items():
-                    self._state = self._state.replace(EN_day, NL_day)
-            if "%A" in date_format:
-                for EN_day, NL_day in DUTCH_TRANSLATION_DAYS.items():
-                    self._state = self._state.replace(EN_day, NL_day)
-            if "%a" in date_format:
-                for EN_day, NL_day in DUTCH_TRANSLATION_DAYS_SHORT.items():
-                    self._state = self._state.replace(EN_day, NL_day)
+            self._state = _translate_state(date_format, self._state)
 
     def __set_sort_date(self, collection):
         self._sort_date = int(collection.date.strftime('%Y%m%d'))
@@ -251,20 +174,23 @@ class WasteTypeSensor(RestoreEntity, SensorEntity):
 
 class WasteDateSensor(RestoreEntity, SensorEntity):
 
-    def __init__(self, data, waste_types, waste_collector, date_delta, dutch_days, name, name_prefix):
+    def __init__(self, data, config, date_delta):
         self.data = data
-        self.waste_types = waste_types
-        self.waste_collector = waste_collector
+        self.waste_types = config[CONF_RESOURCES]
+        self.waste_collector = config.get(CONF_WASTE_COLLECTOR).lower()
+        self.dutch_days = config.get(CONF_TRANSLATE_DAYS)
         self.date_delta = date_delta
-        self.dutch_days = dutch_days
-        if date_delta.days == 0:
-            day = 'vandaag' if self.dutch_days else "today"
-        elif date_delta.days == 1:
-            day = 'morgen' if self.dutch_days else "tomorrow"
+        
+        if self.date_delta.days == 0:
+            day = "vandaag" if self.dutch_days else "today"
+        elif self.date_delta.days == 1:
+            day = "morgen" if self.dutch_days else "tomorrow"
         else:
             day = ''
-        self._name = _format_sensor(name, name_prefix, waste_collector, day)
-        self._attr_unique_id = _format_sensor(name, name_prefix, waste_collector, day)
+            
+        formatted_name = _format_sensor(config.get(CONF_NAME), config.get(CONF_NAME_PREFIX),  self.waste_collector, day)
+        self._name = formatted_name
+        self._attr_unique_id = formatted_name
         self._hidden = False
         self._state = None
         self._attrs = {}
@@ -294,8 +220,8 @@ class WasteDateSensor(RestoreEntity, SensorEntity):
 
         if ATTR_HIDDEN in state.attributes:
             self._attrs = {
-            ATTR_HIDDEN: state.attributes[ATTR_HIDDEN]
-        }
+                ATTR_HIDDEN: state.attributes[ATTR_HIDDEN]
+            }
 
     def update(self):
         date = datetime.now() + self.date_delta
@@ -319,3 +245,19 @@ def _format_sensor(name, name_prefix, waste_collector, sensor_type):
         (name + ' ' if name else "") +
         sensor_type
     )
+
+
+def _translate_state(date_format, state):
+    if "%B" in date_format:
+        for EN_day, NL_day in DUTCH_TRANSLATION_MONTHS.items():
+            state = state.replace(EN_day, NL_day)
+    if "%b" in date_format:
+        for EN_day, NL_day in DUTCH_TRANSLATION_MONTHS_SHORT.items():
+            state = state.replace(EN_day, NL_day)
+    if "%A" in date_format:
+        for EN_day, NL_day in DUTCH_TRANSLATION_DAYS.items():
+            state = state.replace(EN_day, NL_day)
+    if "%a" in date_format:
+        for EN_day, NL_day in DUTCH_TRANSLATION_DAYS_SHORT.items():
+            state = state.replace(EN_day, NL_day)
+    return state
