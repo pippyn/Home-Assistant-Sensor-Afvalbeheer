09ab3c9c58df5ad8c2d5ca1aef8d3a65fe33bf77
Pippijn Stortelder <pippyn@users.noreply.github.com>
2023-04-06 15:32:55 +0200

    Final release

--- custom_components/afvalbeheer/__init__.py
+++ custom_components/afvalbeheer/__init__.py
@@ -1,7 +1,7 @@
 """
 Sensor component for waste pickup dates from dutch and belgium waste collectors
 Original Author: Pippijn Stortelder
-Current Version: 5.1.1 20230406
+Current Version: 5.2.0 20230406
 20220829 - Major change: Added Calendar support (credits @WouterTuinstra)
 20220829 - Give persistant notifications unique id's
 20220901 - Code cleanup
@@ -21,6 +21,7 @@ Current Version: 5.1.1 20230406
 20230228 - Code refactor
 20230303 - New next upcoming sensor
 20230406 - Fix for calendar
+20230406 - New API for RMN and BAR
 
 Example config:
 Configuration.yaml:
