8c496d83da7075d0950a9ada4d2a4b776d7a3e4e
Bram van Dartel <root@rootrulez.com>
2024-01-07 09:33:36 +0100

    add woerden for ximmio

--- custom_components/afvalbeheer/const.py
+++ custom_components/afvalbeheer/const.py
@@ -113,6 +113,7 @@ XIMMIO_COLLECTOR_IDS = {
     'twentemilieu':     '8d97bb56-5afd-4cbc-a651-b4f7314264b4',
     'waardlanden':      '942abcf6-3775-400d-ae5d-7380d728b23c',
     'westland':         '6fc75608-126a-4a50-9241-a002ce8c8a6c',
+    'woerden':          '06856f74-6826-4c6a-aabf-69bc9d20b5a6',
     'reinis':           '9dc25c8a-175a-4a41-b7a1-83f237a80b77',
 }
 
