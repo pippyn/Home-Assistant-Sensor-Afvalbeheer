17978efdcc3e35bf346d493e64cff2f95d6f3518
Pippyn <pippyn@users.noreply.github.com>
2025-12-19 08:49:02 +0100

    Version bumb

--- custom_components/afvalbeheer/__init__.py
+++ custom_components/afvalbeheer/__init__.py
@@ -1,7 +1,7 @@
 """
 Sensor component for waste pickup dates from dutch and belgium waste collectors
 Original Author: Pippijn Stortelder
-Current Version: 6.5.1 20251204
+Current Version: 6.5.2 20251219
 """
 
 import logging
@@ -18,7 +18,7 @@ from .const import DOMAIN, PLATFORM_SCHEMA, CONF_ID, NOTIFICATION_ID, CONF_WASTE
 from .API import get_wastedata_from_config
 
 
-__version__ = "6.5.1"
+__version__ = "6.5.2"
 
 _LOGGER = logging.getLogger(__name__)
 
