4f26f3081cf9667fdfdfbcff03619bf05ee1bc44
Pippyn <pippyn@users.noreply.github.com>
2026-02-03 11:58:26 +0100

    Update RecycleApp mapping

--- custom_components/afvalbeheer/collectors/individual/recycle_app.py
+++ custom_components/afvalbeheer/collectors/individual/recycle_app.py
@@ -22,6 +22,7 @@ class RecycleApp(WasteCollector):
     """
     WASTE_TYPE_MAPPING = {
         'grof': WASTE_TYPE_BULKLITTER,
+        'groot huisvuil': WASTE_TYPE_BULKLITTER,
         # 'glas': WASTE_TYPE_GLASS,
         'glas': WASTE_TYPE_GLASS,
         # 'duobak': WASTE_TYPE_GREENGREY,
